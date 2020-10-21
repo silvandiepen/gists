b981aedf2b8c419b7f7c758f794eec736478aa86
Sil van Diepen <me@sil.mt>
2020-10-21 09:48:38 +0200

    add select feature

--- gig-commit.sh
+++ gig-commit.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+source ./select.sh
+
 branch=$(git symbolic-ref HEAD | sed -e 's,.*/\(.*\),\1,')
 staged=$(git diff --name-only --cached)
 
@@ -27,36 +29,43 @@ printf "\n"
 # echo '\033[38;5;1mnormal \033[02;38;5;1mdim \033[01;38;5;1mbold'
 
 
+# CURRENT TEAM
 branchArray=(${branch//-/ })
-
 printf "\t${bold}Team ID${reset} ${blue}($branchArray)${reset} \n"
 read -p " " TEAM_ANSWER
 TEAM="${TEAM_ANSWER:-$branchArray}"
 
-TICKET_NUMBER="${branch//[^0-9]/}"
 
+# TICKET NUMBER
+TICKET_NUMBER="${branch//[^0-9]/}"
 printf "\t${bold}Ticket Number${reset} ${blue}($TICKET_NUMBER)${reset} \n"
 read -p " " TICKET_ANSWER
 TICKET="${TICKET_ANSWER:-$TICKET_NUMBER}"
 
 
+# PACKAGE
 PACKAGE_FOLDER=$(basename $PWD)
 printf "\t${bold}Package name${reset} ${blue}($PACKAGE_FOLDER)${reset} \n"
 read -p " " PACKAGE_ANSWER
 PACKAGE="${PACKAGE_ANSWER:-$PACKAGE_FOLDER}"
 
 
-TYPE_DEFAULT=$(basename $PWD)
-printf "\t${bold}type${reset} ${blue}($TYPE_DEFAULT)${reset} \n"
-read -p " " TYPE_ANSWER
-TYPE="${TYPE_ANSWER:-$TYPE_DEFAULT}"
+# TYPE OF BRANCH
+printf "\t${bold}type${reset} ${blue}${reset} \n"
+type_options=("feat" "fix" "bugfix" "test")
+select_option "${type_options[@]}"
+choice=$?
+BRANCH_TYPE="${type_options[$choice]}"
 
+
+# COMMIT MESSAGE
 printf "\t${bold}Commit Message${reset} "
 read -p " " MESSAGE_ANSWER
 MESSAGE=`echo "$MESSAGE_ANSWER" | awk '{ print tolower($1) }'`
+COMMIT_MSG="git commit -m \"$BRANCH_TYPE($PACKAGE): $MESSAGE \" -m \"Closes #${TEAM}-${TICKET}\""
 
-COMMIT_MSG="git commit -m \"$TYPE($PACKAGE): $MESSAGE \" -m \"Closes #${TEAM}-${TICKET}\""
 
+# SHOW COMMIT MESSAGE
 printf "\n"
 printf "\t${yellow}$COMMIT_MSG${reset}" 
 printf "\t"
@@ -67,9 +76,9 @@ DO_COMMIT="${COMMIT_ANSWER:-$Y}"
 
 # echo $DO_COMMIT
 
-if [["$DO_COMMIT" = "Y" || "$DO_COMMIT" = 'y']]; then
+if [ "$DO_COMMIT" = 'y' ]; then
     echo 'Gonna commit it to your mother!'
-    # COMMIT_MSG
+$COMMIT_MSG
 else 
     echo "Ok, you don't have to.."
 fi
